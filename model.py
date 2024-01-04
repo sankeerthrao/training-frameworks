6a775672a65e6da0dc93e80576a60e5bd2f4e518
Sankeerth Rao Karingula <sankeerth1729@gmail.com>
2024-01-04 02:08:11 -0800

    Adding deepspeed training example

---
+++ model.py
@@ -0,0 +1,16 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+
+class SimpleNet(nn.Module):
+    def __init__(self):
+        super(SimpleNet, self).__init__()
+        self.fc1 = nn.Linear(784, 256)
+        self.relu = nn.ReLU()
+        self.fc2 = nn.Linear(256, 10)
+
+    def forward(self, x):
+        x = self.fc1(x)
+        x = self.relu(x)
+        x = self.fc2(x)
+        return x
---
+++ train.py
@@ -0,0 +1,36 @@
+import deepspeed
+from torch.utils.data import DataLoader, TensorDataset
+import numpy as np
+
+# Initialize model
+model = SimpleNet()
+
+# Parameters for DeepSpeed
+params = list(model.parameters())
+cmd_args = {
+    'deepspeed_config': 'ds_config.json'
+}
+
+# Initialize DeepSpeed
+model_engine, optimizer, _, _ = deepspeed.initialize(args=cmd_args,
+                                                     model=model,
+                                                     model_parameters=params)
+
+# Dummy dataset
+x = torch.from_numpy(np.random.randn(1000, 784)).float()
+y = torch.from_numpy(np.random.randint(0, 10, (1000,))).long()
+dataset = TensorDataset(x, y)
+data_loader = DataLoader(dataset, batch_size=32)
+
+# Training loop
+for epoch in range(5):
+    for batch in data_loader:
+        inputs, labels = batch
+
+        # Forward pass
+        outputs = model_engine(inputs)
+        loss = nn.CrossEntropyLoss()(outputs, labels)
+
+        # Backward pass and optimization
+        model_engine.backward(loss)
+        model_engine.step()
